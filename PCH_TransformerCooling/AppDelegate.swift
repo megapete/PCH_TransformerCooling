4150220105d3988d28f34879ec6e8ce9264342bf
Peter Huber <peterhub@peters-macbook-pro.local>
2018-09-10 17:53:34 -0400

    Initial Commit

---
+++ PCH_TransformerCooling/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PCH_TransformerCooling
+//
+//  Created by Peter Huber on 2018-09-10.
+//  Copyright © 2018 Huberis Technologies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
