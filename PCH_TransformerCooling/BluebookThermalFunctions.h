d9bee20b6450ec0e991841d3b161dba70adbc245
Peter Huber <peterhub@peters-macbook-pro.local>
2018-09-11 19:53:45 -0400

    Fleshed out the DiskModel class

--- PCH_TransformerCooling/BluebookThermalFunctions.h
+++ PCH_TransformerCooling/BluebookThermalFunctions.h
@@ -14,9 +14,10 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define SPECIFIC_HEAT_OF_OIL        1880.0  // J/kg
-#define FLUID_DENSITY_OF_OIL        867.0   // kg/m3
-#define THERMAL_CONDUCTIVITY_OF_OIL 0.11    // W/mC
+#define SPECIFIC_HEAT_OF_OIL            1880.0  // J/kg
+#define FLUID_DENSITY_OF_OIL            867.0   // kg/m3
+#define THERMAL_CONDUCTIVITY_OF_OIL     0.11    // W/mC
+#define THERMAL_CONDUCTIVITY_OF_PAPER   0.16
 
 /// General function to calculate hydraulic diameter for an arbitrary shape, given the cross-secitonal area and the wetted perimeter
 double HydraulicDiameter(double xSect, double wPerimeter);
--- PCH_TransformerCooling/DiscModel.swift
+++ PCH_TransformerCooling/DiscModel.swift
@@ -14,12 +14,12 @@ class DiscModel: NSObject {
     let dims:(ID:Double, rb:Double, h:Double)
     let paperCover:Double
     
-    let belowGap:Double
-    let aboveGap:Double
+    // let belowGap:Double
+    // let aboveGap:Double
     
     // the per-unit exposed surface / total surface
-    let aboveGapSpaceFactor:Double
-    let belowGapSpaceFactor:Double
+    // let aboveGapSpaceFactor:Double
+    // let belowGapSpaceFactor:Double
     
     let resistance20:Double
     let eddyPU:Double
@@ -38,7 +38,7 @@ class DiscModel: NSObject {
     
     // Things that need to be calculated every time through
     var temperature:Double = 20.0
-    var loss:Double = 0.0
+    // var loss:Double = 0.0
     var hInner:Double = 0.0
     var hOuter:Double = 0.0
     var hAbove:Double = 0.0
@@ -50,12 +50,12 @@ class DiscModel: NSObject {
         self.dims.rb = radialBuild
         self.dims.h = height
         self.paperCover = paperThickness
-        self.belowGap = belowGap
-        self.aboveGap = aboveGap
+        // self.belowGap = belowGap
+        // self.aboveGap = aboveGap
         self.resistance20 = resistanceAt20C
         self.eddyPU = eddyPU
-        self.aboveGapSpaceFactor = aboveSpaceFactor
-        self.belowGapSpaceFactor = belowSpaceFactor
+        // self.aboveGapSpaceFactor = aboveSpaceFactor
+        // self.belowGapSpaceFactor = belowSpaceFactor
         
         // calculate and store the hydraulic diameters for the horizontal ducts
         let lmt = (innerDiameter + radialBuild) * π
@@ -83,15 +83,57 @@ class DiscModel: NSObject {
         self.Aouter = (innerDiameter + 2.0 * radialBuild) * π * outerSpaceFactor * height
     }
     
-    func Loss(amps:Double, temp:Double) -> Double
+    func Loss(amps:Double) -> Double
     {
-        let resistance = self.resistance20 * (234.5 + temp) / (234.5 + 20) * (1.0 + self.eddyPU)
+        let resistance = self.resistance20 * (234.5 + self.temperature) / (234.5 + 20) * (1.0 + self.eddyPU)
         
         return amps * amps * resistance
     }
     
-    func UpdateTemperature(amps:Double, T1:Double, T2:Double, T3:Double, T4:Double, v12:Double, v34:Double, v13:Double, v24:Double)
+    /// Update the temperature of the disc. The subscript numbers for the velocity (12, 34, etc) are the paths as referenced from disc 1. The subscripts for the temperatures are the nodes around disc 1.
+    func UpdateTemperature(amps:Double, T1:Double, T2:Double, T3:Double, T4:Double, v12:Double, v34:Double, v13:Double, v24:Double) -> Double
     {
+        var oldTemp = self.temperature
+        
+        let Tbelow = (T1 + T2) / 2.0
+        let Tabove = (T3 + T4) / 2.0
+        let Tinner = (T1 + T3) / 2.0
+        let Touter = (T2 + T4) / 2.0
+        
+        repeat {
+        
+            // The losses and the convection coefficient are dependent on the temperature, so we iterate until the calculated temp doesn't change by more than 0.1 degree from one iteration to the next. This is my own decision (ie: it's not in the Bluebook).
+            
+            oldTemp = self.temperature
+        
+            let hConv12 = ConvectionCoefficient(self.Dbelow, self.dims.rb, Tbelow, self.temperature - Tbelow, v12)
+            self.hBelow = HeatTransferCoefficient(hConv12, self.paperCover, THERMAL_CONDUCTIVITY_OF_PAPER)
+            
+            let hConv34 = ConvectionCoefficient(self.Dabove, self.dims.rb, Tabove, self.temperature - Tabove, v34)
+            self.hAbove = HeatTransferCoefficient(hConv34, self.paperCover, THERMAL_CONDUCTIVITY_OF_PAPER)
+            
+            let hConv13 = ConvectionCoefficient(self.Dinner, self.dims.h, Tinner, self.temperature - Tinner, v13)
+            self.hInner = HeatTransferCoefficient(hConv13, self.paperCover, THERMAL_CONDUCTIVITY_OF_PAPER)
+            
+            let hConv24 = ConvectionCoefficient(self.Douter, self.dims.h, Touter, self.temperature - Touter, v24)
+            self.hOuter = HeatTransferCoefficient(hConv24, self.paperCover, THERMAL_CONDUCTIVITY_OF_PAPER)
+            
+            self.temperature = (self.Loss(amps: amps) + self.hBelow * self.Abelow * Tbelow + self.hAbove * self.Aabove * Tabove + self.hInner * self.Ainner * Tinner + self.hOuter * self.Aouter * Touter) / (self.hBelow * self.Abelow + self.hAbove * self.Aabove + self.hInner * self.Ainner + self.hOuter * self.Aouter)
+            
+        
+        } while fabs(self.temperature - oldTemp) > 0.1
+        
+        return self.temperature
         
     }
 }
+
+
+
+
+
+
+
+
+
+
