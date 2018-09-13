b02de6dce3fe7c596651de0be3e09d563bbf90e3
Peter Huber <peterhub@peters-macbook-pro.local>
2018-09-13 16:25:41 -0400

    Finished filling in the PV part of the PV matrix

--- PCH_TransformerCooling/SectionModel.swift
+++ PCH_TransformerCooling/SectionModel.swift
@@ -121,6 +121,7 @@ class SectionModel: NSObject {
         pvm[rowIndex, rowIndex] = 1.0
         B[rowIndex] = pIn - PressureChangeUsingKandD(currentDisc.kInner, currentDisc.Dinner, OilViscosity(self.NodeTemps[0]), currentDisc.verticalPathLength, vIn)
         
+        // We need to be careful with filling the PV matrix. We need to remember that for an arbitrary disc i, the 2i+1 and 2i+2 nodes are the 2i and 2i-1 nodes AFTER i has been incremented. If we just blindly set the matrix row to all 4 nodes around a disc, then the numbers associated those two nodes will be clobbered by the next disc. For that reason, we only solve for P(2i) and P(2i+1) for each disc. We also note that the path 3i+2 is the 3i-1 path after incrementing i, so we won't do that one either. We WILL do the path 3i, and save the equation in THAT row. However, after doing the final disc, we DO need to solve for the final horizontal path (3n+2).
         for i in 1..<n
         {
             // solve for P(2i-1) - P(2i) and stuff it into row 2i
@@ -134,9 +135,42 @@ class SectionModel: NSObject {
             // the velocity moves from the right side of the equation to the left, so we take the negative of the coefficient
             pvm[rowIndex, vOffset + 3*i-1] = -vCoeff
             
+            // now we'll solve for P(2i-1) - P(2i+1) and put it in row 2i+1
+            rowIndex = pOffset + 2*i+1
+            
+            pvm[rowIndex, pOffset + 2*i+1] = -1.0;
+            pvm[rowIndex, pOffset + 2*i-1] = 1.0
+            
+            // The other two paths we solve for are the vertical ones. That means that both K and D depend on whether the oil is coming in on the inner or outer duct.
+            var D = (self.inletLoc == .inner ? currentDisc.Dinner : currentDisc.Douter)
+            var K = (self.inletLoc == .inner ? currentDisc.kInner : currentDisc.kOuter)
+            
+            vCoeff = PressureChangeUsingKandD(K, D, OilViscosity((self.NodeTemps[2*i-1] + self.NodeTemps[2*i+1]) / 2.0), currentDisc.verticalPathLength, 1.0)
+            pvm[rowIndex, vOffset + 3*i+1] = -vCoeff
+            
+            // we now solve for P(2i) - P(2i+2) and stuff it into row v3i (ie: vOffset + 3i)
+            rowIndex = vOffset + 3*i
+            
+            pvm[rowIndex, pOffset + 2*i+2] = -1.0
+            pvm[rowIndex, pOffset + 2*i] = 1.0
+            
+            D = (self.inletLoc == .outer ? currentDisc.Dinner : currentDisc.Douter)
+            K = (self.inletLoc == .outer ? currentDisc.kInner : currentDisc.kOuter)
+            
+            vCoeff = PressureChangeUsingKandD(K, D, OilViscosity((self.NodeTemps[2*i] + self.NodeTemps[2*i+2]) / 2.0), currentDisc.verticalPathLength, 1.0)
+            pvm[rowIndex, vOffset + 3*i] = -vCoeff
+            
             // advance to the next disc in the section
             currentDisc = self.discs[i]
         }
+        
+        // Now do the last path, P(2n+1) - P(2n+2) and store it in row 2n+2
+        rowIndex = pOffset + 2*n+2
+        pvm[rowIndex, pOffset + 2*n+2] = -1.0
+        pvm[rowIndex, pOffset + 2*n+1] = 1.0
+        
+        let vCoeff = PressureChangeUsingKandD(currentDisc.kAbove, currentDisc.Dabove, OilViscosity((self.NodeTemps[2*n+1] + self.NodeTemps[2*n+2]) / 2.0), currentDisc.horizontalPathLength, 1.0)
+        pvm[rowIndex, vOffset + 3*n+2] = -vCoeff
         
         return true
     }
